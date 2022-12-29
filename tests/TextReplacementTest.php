7404af65e819fd1bd49f7d34adc94606549e7e66
Martin Krcho <1962663+martinkrcho@users.noreply.github.com>
2022-12-29 13:53:59 +0100

    Add test for multi-word keywords.

--- tests/TextReplacementTest.php
+++ tests/TextReplacementTest.php
@@ -207,6 +207,40 @@ class TextReplacementTest extends \PHPUnit\Framework\TestCase {
 				. '<a href="https://linking.objav.digital/services/" title="online marketing" class="ifocus-link-nest" rel="help" target="_blank">non</a>, '
 				. 'hendrerit maximus tellus.',
 			),
+
+			'multiword keyword expression' => array(
+				// keywords
+				array(
+					iFocus_Link_Nest_Keyword_Model::build_from_generic_array(
+						array(
+							'efficitur lacus at libero',
+							'online marketing',
+							'help',
+							'https://linking.objav.digital/services/',
+						)
+					),
+				),
+
+				// original text
+				'Etiam eu posuere ex, quis pretium est. Nulla venenatis, ligula in sollicitudin molestie, nibh arcu vehicula velit, sed facilisis urna justo vel turpis. '
+				.'Etiam tempus elit eu pharetra pretium. Sed in ullamcorper nibh, vitae imperdiet turpis. Suspendisse vestibulum interdum purus, a interdum justo elementum ut. '
+				.'Integer hendrerit fringilla bibendum. Maecenas efficitur lacus at libero vestibulum tempor. Nunc tincidunt elementum turpis, vel venenatis nulla hendrerit ut.'
+				.' Mauris porta, ante in tempus dictum, lacus quam convallis nisi, sit amet faucibus metus felis tempor ipsum. Praesent ac blandit felis, eget faucibus ex. '
+				.'Praesent aliquet elit et vulputate ullamcorper. Nam egestas sodales urna, in tincidunt ante elementum vitae. Fusce in arcu et dolor gravida rutrum vitae vel mi.'
+				.' Proin eget felis consectetur, ullamcorper lacus id, convallis nibh.',
+
+				// expected result
+				'Etiam eu posuere ex, quis pretium est. Nulla venenatis, ligula in sollicitudin molestie, nibh arcu vehicula velit, sed facilisis urna justo vel turpis. '
+				.'Etiam tempus elit eu pharetra pretium. Sed in ullamcorper nibh, vitae imperdiet turpis. Suspendisse vestibulum interdum purus, a interdum justo elementum ut. '
+				.'Integer hendrerit fringilla bibendum. Maecenas '
+				. '<a href="https://linking.objav.digital/services/" title="online marketing" class="ifocus-link-nest" rel="help" target="_blank">efficitur lacus at libero</a>'
+				.' vestibulum tempor. Nunc tincidunt elementum turpis, vel venenatis nulla hendrerit ut.'
+				.' Mauris porta, ante in tempus dictum, lacus quam convallis nisi, sit amet faucibus metus felis tempor ipsum. Praesent ac blandit felis, eget faucibus ex. '
+				.'Praesent aliquet elit et vulputate ullamcorper. Nam egestas sodales urna, in tincidunt ante elementum vitae. Fusce in arcu et dolor gravida rutrum vitae vel mi.'
+				.' Proin eget felis consectetur, ullamcorper lacus id, convallis nibh.',
+
+			),
+
 		);
 	}
 }
